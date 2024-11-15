4f1bb0546ba9bb1e113f490d4c2a41fb3a7c1c17
MTejasvi06 <mtejasvi06@gmail.com>
2024-11-15 18:06:26 +0530

    Create Bridge_Crossing.java

---
+++ visa_prep/Bridge_Crossing.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        if(y>z){
+            System.out.print("0");
+        }
+        else{
+            System.out.println((z-y)/x);
+        }
+    }
+}
