4acae05f72177de9ecebc67bb91410e59551919f
MTejasvi06 <mtejasvi06@gmail.com>
2024-11-16 18:53:29 +0530

    Create Ticket_Fever.java

---
+++ visa_prep/Ticket_Fever.java
@@ -0,0 +1,21 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
+            int n=sc.nextInt();
+            int m=sc.nextInt();
+            if(m>=n){
+                System.out.println("0"); 
+            }
+            else{
+                System.out.println(n-m);
+            }
+        }
+    }
+}
